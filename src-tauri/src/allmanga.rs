7273326514a2cc582653153028722514a9f1d0f8
Owca525 <lembkegrazyna@gmail.com>
2024-11-10 12:49:44 +0100

    async

--- src-tauri/src/allmanga.rs
+++ src-tauri/src/allmanga.rs
@@ -4,5 +4,6 @@ pub fn get_anime_search(url: &str) -> Result<String, Box<dyn std::error::Error>>
     let client = Client::new();
     let response = client.get("https://www.example.com").send()?;
     let json = response.text()?;
+    println!("dasd");
     Ok(json)
 }
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,8 @@
 mod allmanga;
 
-#[tauri::command]
+use tauri::command;
+
+#[command]
 fn get_search(name: &str) -> String {
     let mut string = String::new();
     match allmanga::get_anime_search(name) {
@@ -12,10 +14,10 @@ fn get_search(name: &str) -> String {
     format!("{}", string)
 }
 
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![get_search])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
