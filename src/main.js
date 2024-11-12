11cb5d6d85e5ed29f20a134b1d2f62db120a4daa
Dawid Gardzielewski <thelastcinnamonbun@gmail.com>
2024-11-12 18:16:38 +0100

    Naprawione tworzenie JSON'a

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const { invoke } = window.__TAURI__.core;
 async function fetchData() {
   try {
     const response = await invoke('get_search', {"name": "Oshi no ko"});
-    const cleanedJsonString = response.slice(1, -1).replace(/\\\"/g, '"');
+    const cleanedJsonString = response.slice(1, -1);
     console.log(cleanedJsonString)
     // naprawić żeby dobrze konwertowało string do json
     const jsonObject = JSON.parse("{" + cleanedJsonString);
