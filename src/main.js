bac0174b690142e0c6e0a279c63cb11f0fbe8e03
= <=>
2024-11-13 08:08:15 +0100

    Made search read from text input

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ const { invoke } = window.__TAURI__.core;
 
 async function fetchData() {
   try {
-    const response = await invoke('get_search', {"name": "Oshi no ko"});
+    const input = document.getElementById('search_text')
+    const name = input.value
+    const response = await invoke('get_search', {"name": name});
     const cleanedJsonString = response.slice(1, -1);
     console.log(cleanedJsonString)
     // naprawić żeby dobrze konwertowało string do json
