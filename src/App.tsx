3f866b84d34dde1edfba5c15525e65707c93d027
Owca525 <lembkegrazyna@gmail.com>
2024-12-15 17:26:47 +0100

    adding scale method

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@ import Home from "./pages/home";
 import { Settings } from "./pages/settings";
 import { Player } from "./pages/player";
 import { useEffect } from "react";
-import { checkConfig } from "./utils/config";
+import { checkConfig, readConfig } from "./utils/config";
 
 function App() {
+  const changeScale = async () => {
+    const config = await readConfig();
+    document.body.style.transform = `scale(${config?.General.windows.Scale})`
+    document.body.style.transformOrigin = 'top left';
+  }
   useEffect(() => {
     checkConfig()
+    changeScale()
   })
   return (
     <Router>
