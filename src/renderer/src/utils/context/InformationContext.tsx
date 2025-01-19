db9517c0059465c472900434f36f961e6d43921f
Owca525 <example@example.com>
2025-01-19 10:16:30 +0100

    fixing scrolling in information

--- src/renderer/src/utils/context/InformationContext.tsx
+++ src/renderer/src/utils/context/InformationContext.tsx
@@ -12,6 +12,8 @@ const InformationContext: React.FC<{ children: React.ReactNode }> = ({ children 
   showIndormation = (informationData: { anime_id: string }) => {
     setData(informationData);
     setIsShow(true);
+    const container = document.querySelector(".container") as HTMLDivElement
+    if (container) container.style.display = "none"
   };
 
   hideInformation = () => {
