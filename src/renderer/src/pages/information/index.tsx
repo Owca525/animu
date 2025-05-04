b0746af0c576db815cfe7dfebe5e862c8196fb32
Dawid Gardzielewski <thelastcinnamonbun@gmail.com>
2025-05-04 20:44:04 +0200

    oops

--- src/renderer/src/pages/information/index.tsx
+++ src/renderer/src/pages/information/index.tsx
@@ -1,6 +1,5 @@
 import Button from "@renderer/components/buttons"
 import "./information.css"
-<<<<<<< HEAD
 import { useNavigate } from "react-router-dom"
 
 const data = {
@@ -51,10 +50,8 @@ const data = {
         ]
     }
 }
-=======
 import { useLocation, useNavigate } from "react-router-dom"
 import { AnimeData } from "@renderer/utils/GlobalInterface";
->>>>>>> a53d3d53d6b4e45a922051b7d13b60028c23ee87
 
 function information() {
     const navigate = useNavigate()
@@ -65,11 +62,7 @@ function information() {
     return (
         <main className="information">
             <div className="information-banner">
-<<<<<<< HEAD
                 <img className={(data.bannerImage ? "information-banner-image" : "information-banner-image-blur")} src={(data.bannerImage ? data.bannerImage : data.coverImage)}/>
-=======
-                <img className="information-banner-image" src={data.bannerImage}></img>
->>>>>>> a53d3d53d6b4e45a922051b7d13b60028c23ee87
             </div>
             
                 <div className="information-fade"></div>
