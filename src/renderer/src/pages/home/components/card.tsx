f9c7a4d54ed0fdf05065355138f997cc59da32cb
Owca525 <example@gmail.com>
2025-05-04 18:38:04 +0200

    adding card loading

--- src/renderer/src/pages/home/components/card.tsx
+++ src/renderer/src/pages/home/components/card.tsx
@@ -1,9 +1,11 @@
 import { cardData } from "@renderer/utils/GlobalInterface"
 import "./css/card.css"
 import { useNavigate } from "react-router-dom"
+import { useState } from "react"
 
 const Card: React.FC<cardData> = ({ AnimeData, deletionCard }) => {
   const navigate = useNavigate()
+  const [isLoading, setLoading] = useState<boolean>(true)
 
   function sendToInformation() {
     navigate("/info", {
@@ -13,7 +15,8 @@ const Card: React.FC<cardData> = ({ AnimeData, deletionCard }) => {
 
   return (
     <div className="card-container" onClick={sendToInformation}>
-        {AnimeData.coverImage ? <img src={AnimeData.coverImage} className="card-image"/> : <div className="card-image-placeholder"></div>}
+        {AnimeData.coverImage ? <img src={AnimeData.coverImage} className="card-image" onLoad={() => setLoading(() => false)} style={isLoading ? {display: "none"} : {}} /> : ""}
+        {isLoading ? <div className="card-image-placeholder"><span className="material-symbols-outlined home-loading-animation">progress_activity</span></div> : ""}
         <div className="card-title">{AnimeData.title}</div>
     </div>
   )
