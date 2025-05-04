8fe2b86509ae145700a79085a3d194d751b62abc
Owca525 <example@gmail.com>
2025-05-04 19:43:35 +0200

    adding settings button

--- src/renderer/src/pages/home/index.tsx
+++ src/renderer/src/pages/home/index.tsx
@@ -39,6 +39,7 @@ function home() {
             {
                 icon: "settings",
                 text: "Settings",
+                onClick: () => navigate("/settings")
             }
         ]
     }
