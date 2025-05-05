afa7896187d3eaad56c9331ad617ddc11cb9bc55
KartQ <kamil.wojci@interia.pl>
2025-05-05 16:28:42 +0200

    Basic css for checkboxes and inputs

--- src/renderer/src/pages/settings/index.tsx
+++ src/renderer/src/pages/settings/index.tsx
@@ -4,6 +4,19 @@ function settings() {
 
     return (
         <main>
+            <div className="settings-setting-container">
+                Test
+                <input className="checkbox" type="checkbox"></input>
+            </div>
+
+            <br></br>
+
+            <div className="settings-setting-container">
+                Test
+                <div className="input-container">
+                    <input className="input-field" type="text"></input><div className="input-type">%</div>
+                </div>
+            </div>
         </main>
     )
 }
