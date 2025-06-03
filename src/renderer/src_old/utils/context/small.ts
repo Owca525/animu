292d4a8340ee43f81e044375518d65f0d33aedd7
Owca525 <lembkegrazyna@gmail.com>
2025-06-03 23:27:29 +0200

    rewrite Done

--- src/renderer/src_old/utils/context/small.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react'
-import { SettingsConfig } from '../interface'
-import { defaultConfig } from '../filesMange/config'
-
-// This file is for small context
-
-export const configContext = createContext<SettingsConfig>(defaultConfig)
--- src/renderer/src_old/utils/pluginLoader.ts
+++
@@ -1,9 +0,0 @@
-export async function LoadingPluginOfficial() {
-    const pluginFiles = ['test1', "test2"];
-    const loadedPlugins: { main: () => Promise<void>, information: { version: string, name: string } }[] = [];
-    for (const file of pluginFiles) {
-      const plugin = await import(`../../plugins/${file}.tsx`);
-      loadedPlugins.push({ main: plugin.default, information: plugin.information() });
-      plugin.default()
-    }
-} 
