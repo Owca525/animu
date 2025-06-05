f857ef02147adec8b332cbe2c391e5496a8bbb3a
Owca525 <lembkegrazyna@gmail.com>
2025-06-05 11:33:34 +0200

    improve home

--- src/renderer/src/pages/home/components/container.tsx
+++ src/renderer/src/pages/home/components/container.tsx
@@ -4,16 +4,10 @@ import Card from "./card"
 import { useRef } from "react"
 import Button from "@renderer/components/buttons"
 import store from "@renderer/utils/store"
+import { t } from "i18next"
 
 const Container: React.FC<containerData> = ({ title, data, horizontal = false, onTitleClick }) => {
   const container = useRef<HTMLDivElement>(null)
-  // function handleScroll(event) {
-  //   if (event.deltaY === 0) return;
-  //   if (container.current) {
-  //     container.current.scrollLeft += event.deltaY
-  //     console.log(container.current.scrollLeft, event.deltaY)
-  //   }
-  // }
 
   function handleButtonScroll(num: number) {
     if (!container.current) return
@@ -23,13 +17,13 @@ const Container: React.FC<containerData> = ({ title, data, horizontal = false, o
   return (
     <div className="main-container">
         <div className={onTitleClick ? "container-title-click" : "container-title"} onClick={onTitleClick}>{title}</div>
-        <div className="button-container">
-          {horizontal ? <Button icon="chevron_left" ButtonClass="container-left-skip-button" onClick={() => handleButtonScroll(-120)}/> : ""}
+        <div className={"container-button-container" + (data.length <= 0 ? " container-error" : "")}>
+          {horizontal && data.length > 0 ? <Button icon="chevron_left" ButtonClass="container-left-skip-button" onClick={() => handleButtonScroll(-120)}/> : ""}
           <div className={horizontal ? "container-data-horizontal" : "container-data"} ref={container}> {/* onWheel={handleScroll} */}
               {data.length > 0 && data.map((card) => <Card AnimeData={card.AnimeData} saveData={card.saveData} deletionCard={card.deletionCard} />)}
-              {data.length <= 0 && <div>None</div>}
+              {data.length <= 0 && <div className="container-error-text">{t("home.nothingfound")}</div>}
           </div>
-          {horizontal ? <Button icon="chevron_right" ButtonClass="container-right-skip-button" onClick={() => handleButtonScroll(120)}/> : ""}
+          {horizontal && data.length > 0 ? <Button icon="chevron_right" ButtonClass="container-right-skip-button" onClick={() => handleButtonScroll(120)}/> : ""}
         </div>
         {store.getState().home.containerLoading ? <div className="container-loading-container"><span className="container-loading material-symbols-outlined">progress_activity</span></div> : ""}
     </div>
