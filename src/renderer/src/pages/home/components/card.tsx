49afca370d2da96ecb5724e6a84cdeacd709eb2f
Owca525 <lembkegrazyna@gmail.com>
2025-06-15 00:08:30 +0200

    adding error notification in cards

--- src/renderer/src/pages/home/components/card.tsx
+++ src/renderer/src/pages/home/components/card.tsx
@@ -8,6 +8,7 @@ import { useSelector } from "react-redux"
 const Card: React.FC<cardData> = ({ AnimeData, saveData, deletionCard, onClick }) => {
   const navigate = useNavigate()
   const [isLoading, setLoading] = useState<boolean>(true)
+  const [isError, setisError] = useState<boolean>(false)
   const pluginPlayer = useSelector((plugin: any) => plugin.plugin.playerPlugin);
 
   async function sendToInformation() {
@@ -41,10 +42,17 @@ const Card: React.FC<cardData> = ({ AnimeData, saveData, deletionCard, onClick }
     if (deletionCard) deletionCard()
   }
 
+  function checkState() {
+    if (isLoading) return { display: "none" }
+    if (isError) return { display: "none" }
+    return {}
+  }
+
   return (
-    <div className="card-container" onClick={sendToInformation}>
-      {AnimeData.coverImage ? <img src={AnimeData.coverImage} className="card-image" onLoad={() => setLoading(() => false)} style={isLoading ? { display: "none" } : {}} /> : ""}
-      {isLoading ? <div className="card-image-placeholder"><span className="material-symbols-outlined home-loading-animation">progress_activity</span></div> : ""}
+    <div className="card-container" onClick={sendToInformation} title={AnimeData.title}>
+      {AnimeData.coverImage && <img src={AnimeData.coverImage} className="card-image" onLoad={() => setLoading(() => false)} onError={() => setisError(() => true)} style={checkState()} />}
+      {isLoading && isError == false && <div className="card-image-placeholder"><span className="material-symbols-outlined home-loading-animation">progress_activity</span></div>}
+      {isError && <div className="card-image-placeholder"><span className="material-symbols-outlined">error</span></div>}
       <div className="card-title">{AnimeData.title}</div>
       {saveData && saveData.episode && <div className="card-continue-watch-text">{saveData.last_Time != 0 && saveData.type != "" ? t("history.continue", { ep: saveData.episode }) : t("history.history", { ep: saveData.episode })}</div>}
       {deletionCard && <div className="card-delete-icon material-symbols-outlined" onClick={runDeletionFunction}>close</div>}
