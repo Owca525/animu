9921f164ca7d94e79a515db00289e6aa8d9b046f
Owca525 <lembkegrazyna@gmail.com>
2025-06-20 11:08:21 +0200

    adding animation in dropdown

--- src/renderer/src/pages/information/components/drop.tsx
+++ src/renderer/src/pages/information/components/drop.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import "./css/drop.css"
+import { motion } from 'framer-motion'
 
 interface dropData {
     content: React.ReactNode
@@ -10,13 +11,18 @@ interface dropData {
 const Drop: React.FC<dropData> = ({ content, LeftHeader, RightHeader }) => {
     const [isOpen, setIsOpen] = useState(false)
 
+    const dropVariants = {
+        hidden: { opacity: 0, height: 0 },
+        visible: { opacity: 1, height: "max-content" },
+    };
+
     return (
         <div className="drop-container">
             <div className={`drop-button ${isOpen ? "active-drop" : ""}`} onClick={() => setIsOpen((prev) => !prev)}>
                 <div className="drop-header">{LeftHeader}</div>
                 <div className="drop-header">{RightHeader} <div className="episodes-arrow material-symbols-outlined">{isOpen ? "arrow_drop_up" : "arrow_drop_down"}</div></div>
             </div>
-            <div className={`drop-content ${isOpen ? "drop-content-show" : ""}`}>{content}</div>
+            <motion.div variants={dropVariants} initial={"hidden"} animate={isOpen ? "visible" : "hidden"} transition={{duration: 0.2}} className={`drop-content ${isOpen ? "drop-content-show" : ""}`}>{content}</motion.div>
         </div>
     )
 }
