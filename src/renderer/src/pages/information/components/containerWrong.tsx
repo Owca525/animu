de939eacf89902a5ec30de412319964378b1319e
Owca525 <lembkegrazyna@gmail.com>
2025-06-23 11:42:57 +0200

    improve wrong container

--- src/renderer/src/pages/information/components/containerWrong.tsx
+++ src/renderer/src/pages/information/components/containerWrong.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import Container from '../../home/components/container'
 import { useSelector } from 'react-redux';
 import { useQuery } from 'react-query';
 import './css/containerWrong.css'
 import Button from '@renderer/components/buttons';
 import { motion } from 'framer-motion';
+import { t } from 'i18next';
+import Card from '@renderer/pages/home/components/card';
+import { cardData } from '@renderer/utils/GlobalInterface';
 
 interface containerwrongprops {
     name: string,
@@ -24,10 +26,21 @@ const containerWrong: React.FC<containerwrongprops> = ({ name, exitfunc, refetch
         }
     );
 
+    function buildContainer(cardata: cardData[]) {
+        return (
+            <div className='information-containerwrong-container'>
+                <div className='information-containerwrong-title'>{t("information.containerwrong.title", { name: name })}</div>
+                <div className="information-containerwrong-cards">
+                    {cardata.map(card => <Card AnimeData={card.AnimeData} deletionCard={card.deletionCard} onClick={card.onClick} />)}
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div className='information-containerwrong-void'>
             <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.2 }} className='information-containerwrong'>
-                {isLoading == false && isError == false && data && <Container title={`Anime Found For name: ${name}`} data={data.map((anime) => {return {...anime, onClick: () => refetchfunc(pluginPlayer.player.animeDataList, undefined, anime.AnimeData.player_ID)}})} />}
+                {isLoading == false && isError == false && data && buildContainer(data.map((anime) => {return {...anime, onClick: () => refetchfunc(pluginPlayer.player.animeDataList, undefined, anime.AnimeData.player_ID)}}))}
                 {isLoading && <div className='information-containerwrong-loading'><span className='information-loading material-symbols-outlined'>progress_activity</span></div>}
                 {isError && isLoading == false && <div className='information-containerwrong-error'><span className='material-symbols-outlined'>error</span>Error Accured</div>}
                 <Button icon="arrow_back" ButtonClass="information-containerwrong-exit-button" onClick={exitfunc} />
