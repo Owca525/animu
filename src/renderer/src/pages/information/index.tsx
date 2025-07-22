90e7e365dd830328f85a28f3489ead48386679a0
Owca525 <lembkegrazyna@gmail.com>
2025-07-22 11:10:26 +0200

    kadsfadijopsu[fhbdjklsz:Fbnadfhjkls;gbbadfhijklos;gabdfghjklsabdfghjklnsabdfgjklnsh;abdfgsjklnbjkln;

--- src/renderer/src/pages/information/index.tsx
+++ src/renderer/src/pages/information/index.tsx
@@ -139,9 +139,10 @@ function information() {
                     <div className="information-top">
                         <div className="information-image-container">
                             {anime_data.averageScore && <div title="Anime Score" className="information-score" style={{ border: `3px solid ${getGradientColor(anime_data.averageScore)}` }}>{anime_data.averageScore}%</div>}
-                            <img className="information-cover" onClick={() => anime_data.coverImage && SaveCoverToClipboard(anime_data.coverImage)} onError={() => setCoverIsError(() => true)} onLoad={() => setCoverIsLoading(() => false)} src={anime_data.coverImage ? anime_data.coverImage : ""} style={isCoverLoading ? { display: "none" } : isError ? { display: "none" } : { animation: "fadeIn 0.3s forwards" }}></img>
+                            {/* <img className="information-cover" onClick={() => anime_data.coverImage && SaveCoverToClipboard(anime_data.coverImage)} onError={() => setCoverIsError(() => true)} onLoad={() => setCoverIsLoading(() => false)} src={anime_data.coverImage ? anime_data.coverImage : ""} style={isCoverLoading ? { display: "none" } : isError ? { display: "none" } : { animation: "fadeIn 0.3s forwards" }}></img>
                             {isCoverLoading && isError == false && <div className="information-cover-placeholder"><span className="material-symbols-outlined home-loading-animation">progress_activity</span></div>}
-                            {isCoverError && isCoverLoading == false && <div className="information-cover-placeholder"><span className="material-symbols-outlined">error</span></div>}
+                            {isCoverError && isCoverLoading == false && <div className="information-cover-placeholder"><span className="material-symbols-outlined">error</span></div>} */}
+                            <div className="information-cover-placeholder"><span className="material-symbols-outlined home-loading-animation">progress_activity</span></div>
 
                             <div className="information-title-mobile-container">
                                 <div className="information-title-mobile">{anime_data.title.romaji}</div>
