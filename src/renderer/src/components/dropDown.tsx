95cb3d44aca59afd2928acd9c25422acc6a79a71
Owca525 <lembkegrazyna@gmail.com>
2025-08-03 14:40:41 +0200

    adding support icon in dropdown

--- src/renderer/src/components/dropDown.tsx
+++ src/renderer/src/components/dropDown.tsx
@@ -4,6 +4,7 @@ import "./css/dropDown.css"
 interface DropdownOption {
   label: string
   onClick?: (text: string) => void
+  icon?: string
 }
 
 interface DropdownProps {
@@ -18,6 +19,7 @@ interface DropdownProps {
 const Dropdown: React.FC<DropdownProps> = ({ options, placeholder = '', placeholderChange, buttonText = "", onClickX, disableX = false }) => {
   const [isOpen, setIsOpen] = useState(false)
   const [text, setText] = useState<string>(buttonText)
+  const [icon, setIcon] = useState<string | undefined>(undefined)
 
   const toggleDropdown = () => setIsOpen(prev => !prev)
 
@@ -25,6 +27,7 @@ const Dropdown: React.FC<DropdownProps> = ({ options, placeholder = '', placehol
     setIsOpen(false)
     if (option.onClick) option.onClick(option.label)
     setText(() => option.label)
+    setIcon(() => option.icon)
     if (placeholderChange) setText(() => placeholderChange())
   }
 
@@ -37,7 +40,7 @@ const Dropdown: React.FC<DropdownProps> = ({ options, placeholder = '', placehol
   return (
     <div className="dropdown-container">
       <div className={`dropdown-button`} onClick={toggleDropdown}>
-        <div className={`dropdown-button-text ${text == "" ? "dropdown-button-shadow-text" : ""}`}>{text == "" ? placeholder : text }</div>
+        <div className={`dropdown-button-text ${text == "" ? "dropdown-button-shadow-text" : ""}`}>{icon && <img className='dropdown-item-image' src={icon}></img>} {text == "" ? placeholder : text }</div>
         {text == "" && <div className='material-symbols-outlined dropdown-button-icon'>{isOpen ? "keyboard_arrow_left" : "keyboard_arrow_down"}</div>}
         {text != "" && !disableX && <div className='material-symbols-outlined dropdown-button-icon' onClick={resetText}>close</div>}
       </div>
@@ -50,6 +53,7 @@ const Dropdown: React.FC<DropdownProps> = ({ options, placeholder = '', placehol
               onClick={() => handleOptionClick(option)}
               title={option.label}
             >
+              {option.icon && <img className='dropdown-item-image' src={option.icon} />}
               {option.label}
             </li>
           ))}
