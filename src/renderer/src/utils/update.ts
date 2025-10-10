6c50c626a29e21d120cfe32865b057db6c3ac354
Owca525 <lembkegrazyna@gmail.com>
2025-10-10 17:16:46 +0200

    fixing update notification and mispeling word

--- src/renderer/src/utils/update.ts
+++ src/renderer/src/utils/update.ts
@@ -6,6 +6,7 @@ import store from "./store";
 
 export async function checkUpdate(alwaysShow: boolean = false) {
     let update = await window.api.update.checkUpdate()
+    console.log(update)
     if (update.available) {
         toast.info(t('update.available', { ver: update.version }), { ...notificationProps, onClick: () => { window.api.update.downloadUpdate(); downloadUpdate(toast.loading(t("update.progress", { procent: 0 }), notificationProps)) } });
     } else if (alwaysShow) {
