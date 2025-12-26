b6d042e7965bdd4da6c1896f6dc8a7dc81684c5b
Owca525 <lembkegrazyna@gmail.com>
2025-12-26 20:34:13 +0100

    fixing episode dosen't change

--- src/renderer/src/pages/player/VideoPlayer.tsx
+++ src/renderer/src/pages/player/VideoPlayer.tsx
@@ -678,7 +678,7 @@ const VideoPlayer: Component<VideoPlayerProps> = ({ player_data, anime_data, tem
     })
 
     function setEpisode(type: "next" | "prev") {
-        let ep = temp.episodes.findIndex((item) => item.ep === temp.episode)
+        let ep = temp.episodes.findIndex((item) => item.ep.toString() == temp.episode,toString())
         if (ep < 0) return
         if (type == 'prev') ep = ep - 1
         if (type == 'next') ep = ep + 1
