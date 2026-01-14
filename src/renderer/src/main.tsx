2635b4d9ebd65240f95c6b610a0bd256764aa163
Owca525 <lembkegrazyna@gmail.com>
2026-01-14 14:26:37 +0100

    fixing bug

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,6 +1,5 @@
 import { render } from 'solid-js/web'
 import App from './App'
-import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
 import { DialogProvider } from "./utils/context/DialogContext";
 import { ContextMenu } from "./utils/context/ContextMenu";
 import { ToastProvider } from './utils/context/ToastNotification';
@@ -10,25 +9,15 @@ import { MenuContextProvider } from './utils/context/menuContext';
 import "material-symbols/material-symbols-outlined.woff2"
 import "material-symbols/outlined.css"
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000,
-    },
-  },
-})
-
 render(
   () => (
     <I18nProvider config={{ defaultLang: "en", fallbackLang: "en" }}>
       <MenuContextProvider>
         <DialogProvider>
           <ContextMenu>
-            <QueryClientProvider client={queryClient}>
-              <ToastProvider>
-                <App />
-              </ToastProvider>
-            </QueryClientProvider>
+            <ToastProvider>
+              <App />
+            </ToastProvider>
           </ContextMenu>
         </DialogProvider>
       </MenuContextProvider>
