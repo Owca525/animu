b7d1b9673a46f66bef52f099a9b3bb14ce31018d
Owca525 <lembkegrazyna@gmail.com>
2026-01-26 14:03:50 +0100

    improving lang

--- src/renderer/src/pages/player/components/nerdStats.tsx
+++ src/renderer/src/pages/player/components/nerdStats.tsx
@@ -1,3 +1,4 @@
+import { t } from "@renderer/utils/i18n"
 import "./css/nerdStats.css"
 
 interface nerdStatsProps {
@@ -10,11 +11,11 @@ interface nerdStatsProps {
 export default function nerdStats(props: nerdStatsProps) {
     return (
         <div class="player-nerd-container">
-            <div class="player-nerd-element">Frames: <span class="player-nerd-element">{props.frames.totalVideoFrames}</span></div>
-            <div class="player-nerd-element">Dropped Frames: <span class="player-nerd-element">{props.frames.droppedVideoFrames}</span></div>
-            <div class="player-nerd-element">Volume: <span class="player-nerd-element">{props.volume}%</span></div>
-            <div class="player-nerd-element">CurrentTime: <span class="player-nerd-element">{props.currentTime.toFixed(2)}</span></div>
-            <div class="player-nerd-element">Duration: <span class="player-nerd-element">{props.duration.toFixed(2)}</span></div>
+            <div class="player-nerd-element">{t("player.nerdstats.frames")} <span class="player-nerd-element">{props.frames.totalVideoFrames}</span></div>
+            <div class="player-nerd-element">{t("player.nerdstats.dframes")} <span class="player-nerd-element">{props.frames.droppedVideoFrames}</span></div>
+            <div class="player-nerd-element">{t("player.nerdstats.volume")} <span class="player-nerd-element">{props.volume}%</span></div>
+            <div class="player-nerd-element">{t("player.nerdstats.currentime")} <span class="player-nerd-element">{props.currentTime.toFixed(2)}</span></div>
+            <div class="player-nerd-element">{t("player.nerdstats.durration")} <span class="player-nerd-element">{props.duration.toFixed(2)}</span></div>
         </div>
     )
 }
