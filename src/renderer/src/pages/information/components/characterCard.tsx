54c3c88664997944fa33cf164c29b8dbeaaab326
Owca525 <lembkegrazyna@gmail.com>
2026-02-06 20:47:24 +0100

    improving character cards

--- src/renderer/src/pages/information/components/characterCard.tsx
+++ src/renderer/src/pages/information/components/characterCard.tsx
@@ -1,3 +1,4 @@
+import { createSignal, Match, Switch } from "solid-js";
 import "./css/characterCard.css"
 
 export interface characterCardsProps {
@@ -6,13 +7,39 @@ export interface characterCardsProps {
     image: string,
     role: string,
     onClick?: () => void,
-    ref?: HTMLDivElement 
+    ref?: HTMLDivElement
 }
 
 export default function CharacterCards(props: characterCardsProps) {
+    const [isLoading, setLoading] = createSignal(true);
+    const [isError, setIsError] = createSignal(false);
+
+    function checkState() {
+        if (isLoading()) return { display: "none" };
+        if (isError()) return { display: "none" };
+        return { animation: "fadeIn 0.3s forwards" };
+    }
+
     return (
         <div class="information-characters-card" onClick={props.onClick} ref={props.ref}>
-            <img class="information-characters-card-cover" src={props.image}></img>
+            <Switch>
+                <Match when={isLoading()}>
+                    <div class="information-character-image-placeholder">
+                        <span class="material-symbols-outlined loading-animation icon">progress_activity</span>
+                    </div>
+                </Match>
+                <Match when={isError()}>
+                    <div class="information-character-image-placeholder">
+                        <span class="material-symbols-outlined icon">error</span>
+                    </div>
+                </Match>
+            </Switch>
+            <img class="information-characters-card-cover" 
+                src={props.image} 
+                style={checkState()}
+                onLoad={() => setLoading(false)}
+                onError={() => setIsError(true)}
+            />
             <div class="information-characters-card-description">
                 <span class="information-character-name">{props.name}</span>
                 <span class="information-character-role">{props.role}</span>
