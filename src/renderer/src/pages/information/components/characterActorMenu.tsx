0bf378142bc264a172d1e820b3541a69942f0946
Owca525 <lembkegrazyna@gmail.com>
2026-04-30 11:09:08 +0200

    tmp

---
+++ src/renderer/src/pages/information/components/characterActorMenu.tsx
@@ -0,0 +1,14 @@
+import { createSignal, JSX } from "solid-js";
+import "./css/characterActorMenu.css";
+
+interface characterActorMenuProps {
+
+}
+
+// TODO: END THIS
+export default function characterActorMenu(props: characterActorMenuProps) {
+
+  return (
+    <></>
+  );
+};
