12ab7af3774b2f8b4178a31714132728ebad1de7
Owca525 <lembkegrazyna@gmail.com>
2026-05-31 14:39:41 +0200

    fixing animetsu

--- src/renderer/src/plugins/animetsu.ts
+++ src/renderer/src/plugins/animetsu.ts
@@ -23,19 +23,21 @@ const HEADER = {
 }
 
 const playerHeader = {
-  "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:150.0) Gecko/20100101 Firefox/150.0",
+  "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36",
   Accept: "*/*",
   "Accept-Language": "en-US,en;q=0.9",
   "Accept-Encoding": "gzip, deflate, br, zstd",
   Origin: WEBSITE,
   Referer: WEBSITE,
-  "Sec-GPC": "1",
   "Sec-Fetch-Dest": "empty",
   "Sec-Fetch-Mode": "cors",
   "Sec-Fetch-Site": "cross-site",
-  DNT: "1",
-  Connection: "keep-alive",
-  TE: "trailers",
+  "cache-control": "no-cache",
+  "pragma": "no-cache",
+  "priority": "u=1, i",
+  "sec-ch-ua": `"Not/A)Brand";v="99", "Chromium";v="148"`,
+  "sec-ch-ua-mobile": "?0",
+  "sec-ch-ua-platform": `"Linux"`
 };
 
 function preaperURL(str: string) {
@@ -166,7 +168,7 @@ export function dateToUnix(dateStr: string | undefined): number | undefined {
 
 export default class Animetsu implements playerPluginFormat {
     metadata: playerPluginFormat["metadata"] = {
-        version: "2.1",
+        version: "2.2",
         name: "Animetsu.Live",
         icon: `${WEBSITE}/android-chrome-192x192.png`,
         author: "Owca525",
@@ -192,12 +194,10 @@ export default class Animetsu implements playerPluginFormat {
         const url = URL.createObjectURL(blob);
         const worker = new Worker(url);
         worker.onmessage = (event) => {
-            console.log(event["data"])
-            if (event["data"]["api"] && event["data"]["proxy"]) (window as any).animetsuBackend = {
+            if (event["data"]["proxy"]) (window as any).animetsuBackend = {
                 ...event["data"],
                 api: BACKEND
             }
-            console.log((window as any).animetsuBackend)
             worker.terminate()
         }
         worker.onerror = (event) => {
