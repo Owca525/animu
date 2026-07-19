19ecf5470fc1c43aa2aa54a8c964c49be0e63a53
Owca525 <lembkegrazyna@gmail.com>
2026-07-19 21:23:12 +0200

    adding template

---
+++ src/renderer/src/plugins/template.ts
@@ -0,0 +1,32 @@
+// DISSABLE
+import { request } from "@renderer/utils/functions";
+import { AnimeData, cardData, episodeList, episodeMetadata, FilterPluginsParams, playerData, playerPluginFormat, serverStatusData } from "@renderer/utils/types";
+
+const WEBSITE = ""
+
+export default class template implements playerPluginFormat {
+    metadata: playerPluginFormat["metadata"] = {
+        version: "1.0",
+        name: "template",
+        author: "Owca525",
+        supportLang: ["en"],
+        urlWebsite: WEBSITE,
+        type: "player"
+    };
+    extractPlayerData = async(type: string, episode: episodeMetadata, id: string): Promise<playerData[]> => {
+        return []
+    }
+    extractEpisodeList = async (animeData?: AnimeData, anime_id?: string): Promise<episodeList | undefined> => {
+        return
+    }
+    extractOnlyEpisodesList = async (type: string, anime_id: string): Promise<{ ep: string; img?: string; title?: string; }[]> => {
+        return []
+    }
+    searchAnime = async (name: string, _page: number, _params?: FilterPluginsParams): Promise<cardData[]> => {
+        return []
+    }
+    
+    raportStatus = async (): Promise<{ search: serverStatusData; player: serverStatusData; episodes: serverStatusData; }> => {
+        return undefined as any
+    }
+}
