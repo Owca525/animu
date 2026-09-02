594a50fe9be8784d412ac4c96e04973635568fbb
Owca525 <lembkegrazyna@gmail.com>
2026-09-02 23:15:58 +0200

    fixing bug

--- src/renderer/src/utils/functions.ts
+++ src/renderer/src/utils/functions.ts
@@ -416,7 +416,7 @@ export function updateObject<T, U>(path: string, value: U, object: T): T {
     return newObject
 }
 
-export async function request(url: string, options?: RequestInit, noCors: boolean = false): Promise<{ text: string, json: { [key: string]: any } | undefined, buffer: Buffer, status: number, statusText: string, url: string, success: boolean, responseHeader: Map<string, string> }> {
+export async function request(url: string, options: RequestInit = {}, noCors: boolean = false): Promise<{ text: string, json: { [key: string]: any } | undefined, buffer: Buffer, status: number, statusText: string, url: string, success: boolean, responseHeader: Map<string, string> }> {
     options = JSON.parse(JSON.stringify(options))
     url = unwrap(url)
     try {
